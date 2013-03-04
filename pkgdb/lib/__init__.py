729ea72846d5079db5037e6ce3d8a31ae6012e16
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-03-04 10:58:03 +0100

    Add the creation_session at the top of the lib module

--- pkgdb/lib/__init__.py
+++ pkgdb/lib/__init__.py
@@ -18,3 +18,23 @@
 # License and may only be used or replicated with the express permission
 # of Red Hat, Inc.
 #
+
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+
+def create_session(db_url, debug=False, pool_recycle=3600):
+    """ Create the Session object to use to query the database.
+
+    :arg db_url: URL used to connect to the database. The URL contains
+    information with regards to the database engine, the host to connect
+    to, the user and password and the database name.
+      ie: <engine>://<user>:<password>@<host>/<dbname>
+    :kwarg debug: a boolean specifying wether we should have the verbose
+        output of sqlalchemy or not.
+    :return a Session that can be used to query the database.
+    """
+    engine = sqlalchemy.create_engine(db_url, echo=debug,
+        pool_recycle=pool_recycle)
+    scopedsession = scoped_session(sessionmaker(bind=engine))
+    return scopedsession
