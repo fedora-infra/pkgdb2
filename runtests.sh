86504f792a55a290b930f0c05109f9a0a8b532b1
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-03-28 18:22:13 +0100

    No configuration file specific to the tests for the moment

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-PKGDB_CONFIG=../tests/pkgdb_test.cfg PYTHONPATH=pkgdb nosetests \
+#PKGDB_CONFIG=../tests/pkgdb_test.cfg PYTHONPATH=pkgdb nosetests \
+PYTHONPATH=pkgdb nosetests \
 --with-coverage --cover-erase --cover-package=pkgdb $*
