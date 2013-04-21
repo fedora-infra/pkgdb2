06447c31021c453bdc14b6b7f843157468092f26
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-04-21 19:33:08 +0200

    Expand the python API
    Add method to retrieve the list of pending ACLs requested on a specific package by a specific user

--- pkgdb/lib/__init__.py
+++ pkgdb/lib/__init__.py
@@ -348,7 +348,7 @@ def update_collection_status(session, clt_branchname, clt_status):
                              '"%s".' % clt_branchname)
 
 
-def get_pending_acl(session, user):
+def get_pending_acl_user(session, user):
     """ Return the pending ACLs on any of the packages owned by the
     specified user.
     The method returns a list of dictionnary containing the package name
@@ -370,4 +370,30 @@ def get_pending_acl(session, user):
              'status': package.status,
              }
         )
+    return output
+
+
+def get_pending_acl_user_package(session, user, package):
+    """ Return the pending ACLs on a specified package for the specified
+    user.
+    The method returns a list of dictionnary containing the package name
+    the collection branchname, the requested ACL and the user that
+    requested that ACL.
+
+    :arg session: session with which to connect to the database
+    :arg user: the user owning the packages on which to retrieve the
+        list of pending ACLs.
+    """
+    output = []
+    for package in model.PersonPackageListingAcl.get_pending_acl_package(
+            session, user, package):
+        if package.personpackagelist.user == user:
+            output.append(
+                {'package': package.personpackagelist.packagelist.package.name,
+                 'user': package.personpackagelist.user,
+                 'collection': package.personpackagelist.packagelist.collection.branchname,
+                 'acl': package.acl,
+                 'status': package.status,
+                 }
+            )
     return output
