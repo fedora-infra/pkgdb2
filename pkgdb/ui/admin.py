cfb8b4820451e8a3518dd92616ba9caf938e789b
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-09-11 11:18:38 +0200

    Add an admin panel giving access to the logs browser

---
+++ pkgdb/ui/admin.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2013  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2, or (at your option) any later
+# version.  This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY expressed or implied, including the
+# implied warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.  You
+# should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# Any Red Hat trademarks that are incorporated in the source
+# code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission
+# of Red Hat, Inc.
+#
+
+'''
+Admin interface for the Flask application.
+'''
+
+import itertools
+import flask
+
+from dateutil import parser
+from math import ceil
+from sqlalchemy.orm.exc import NoResultFound
+
+import pkgdb.forms
+import pkgdb.lib as pkgdblib
+from pkgdb import SESSION, APP, is_admin
+from pkgdb.ui import UI
+
+
+@UI.route('/admin/')
+@is_admin
+def admin():
+    ''' Index page of the admin interface. '''
+    return flask.render_template('admin.html')
+
+
+@UI.route('/admin/log/')
+@is_admin
+def admin_log():
+    """ Return the logs as requested by the user. """
+    cnt_logs = pkgdblib.search_logs(SESSION, count=True)
+
+    from_date = flask.request.args.get('from_date', None)
+    package = flask.request.args.get('package', None)
+    limit = flask.request.args.get('limit', APP.config['ITEMS_PER_PAGE'])
+    page = flask.request.args.get('page', 1)
+
+    try:
+        page = int(page)
+    except ValueError:
+        page = 1
+
+    try:
+        int(limit)
+    except ValueError:
+        limit = APP.config['ITEMS_PER_PAGE']
+        flask.flash('Incorrect limit provided, using default', 'errors')
+
+    if from_date:
+        try:
+            from_date = parser.parse(from_date).date()
+        except ValueError:
+            flask.flash('Incorrect from_date provided, using default', 'errors')
+            from_date = None
+
+    logs = []
+    try:
+        logs = pkgdblib.search_logs(
+            SESSION,
+            package=package or None,
+            from_date=from_date,
+            page=page,
+            limit=limit,
+            )
+    except pkgdblib.PkgdbException, err:
+        flask.flash(err, 'errors')
+
+    total_page = int(ceil(cnt_logs / float(limit)))
+
+    return flask.render_template(
+        'list_logs.html',
+        logs=logs,
+        cnt_logs=cnt_logs,
+        total_page=total_page,
+        page=page,
+        package=package or '',
+        from_date=from_date or '',
+    )
+
