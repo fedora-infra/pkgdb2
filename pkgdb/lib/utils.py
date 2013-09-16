a704bfc8da635aaac3bf438edf274f8d1e1f6b9d
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-09-16 11:34:30 +0200

    Update the utils function
    - Use dogpile to cache user information and group information from FAS (up to 1 hour).
    - Add method to update the Package owner in bugzilla when PoC changes.

--- pkgdb/lib/utils.py
+++ pkgdb/lib/utils.py
@@ -23,187 +23,59 @@
 Utilities for all classes to use
 '''
 
-import sys
-import os
-import gzip
-import bz2
-import subprocess
-import logging
-
-try:
-    # Note: Ran profile of crc32, md5, sha1, sha256 over our key values
-    # crc32 is almost twice as fast.
-    # md5, sha1, sha256 are comparable to each other
-    # No collisions in our current set
-    from hashlib import sha1 as Hasher
-except ImportError:
-    from sha import new as Hasher
-
-import rpm
-import rpmUtils.transaction
-
-from sqlalchemy import select, and_
-from sqlalchemy.exc import DataError
-
+import pkgdb
 from bugzilla import Bugzilla, RHBugzilla3
-import memcache
 
 # The Fedora Account System Module
 from fedora.client.fas2 import AccountSystem
 
-from pkgdb.lib.model.statuses import StatusTranslation
-from pkgdb import _
 
-MEMCACHE = memcache.Client(config.get('memcached.servers', []))
-
-STATUS = None
-fas = None
-LOG = None
+# Have a global connection to bugzilla open.
 _bugzilla = None
-
-# Get the admin group if one is specified.
-admin_grp = config.get('pkgdb.admin_grp', 'cvsadmin')
-
-# Get the packager group if one is specified.
-pkger_grp = config.get('pkgdb.pkger_grp', 'packager')
-
-# Get the moderator group if one is specified.
-mod_grp = config.get('pkgdb.mod_grp', 'sysadmin')
-
-# Get the provenpackager group if one is specified.
-provenpkger_grp = config.get('pkgdb.provenpkger_grp', 'provenpackager')
-
-# Get the newpackager group if one is specified.
-newpkger_grp = config.get('pkgdb.newpkger_grp', 'newpackager')
-
-# Get the critpath groups if specfied
-critpath_grps = config.get('pkgdb.critpath_grps', (admin_grp,))
+# Have a global connection to FAS open.
+_fas = None
 
 
-def to_unicode(obj, encoding='utf-8', errors='replace'):
-    '''return a unicode representation of the object.
-
-    :arg obj: object to attempt to convert to unicode.  Note: If this is
-        not a str or unicode object then the conversion might not be
-        what you want (as it converts the __str__ of the obj).
-    :kwarg encoding: encoding of the byte string to convert from.
-    :kwarg errors:
-        :strict: raise an error if it won't convert
-        :replace: replace non-converting chars with a certain char for
-            the encoding.  (For instance, ascii substitutes a ?).
-        :ignore: silently drop the bad characters
-
+def get_fas():
+    ''' Retrieve a connection to the Fedora Account System.
     '''
-    if isinstance(obj, unicode):
-        return obj
-    if isinstance(obj, str):
-        return unicode(obj, encoding, errors)
-    return unicode(obj)
+    global _fas
+    if _fas:
+        return _fas
+
+    # Get a connection to FAS
+    fas_url = pkgdb.APP.config['PKGDB_FAS_URL']
+    if not fas_url:
+        raise pkgdb.lib.PkgdbException('No PKGDB_FAS_URL configured')
+
+    fas_user = pkgdb.APP.config['PKGDB_FAS_USER']
+    if not fas_user:
+        raise pkgdb.lib.PkgdbException('No PKGDB_FAS_USER configured')
+    fas_pass = pkgdb.APP.config['PKGDB_FAS_PASSWORD']
+    if not fas_pass:
+        raise pkgdb.lib.PkgdbException(
+            'No PKGDB_FAS_PASSWORD configured')
+
+    _fas = AccountSystem(fas_url, username=fas_user, password=fas_pass,
+            cache_session=False)
 
 
-class UserCache(dict):
-    '''Naive cache for user information.
-
-    This cache can go out of date so use with caution.
-
-    Use clear() to remove all entries from the cache.
-    Use del cache[username] to remove a specific entry.
+@pkgdb.cache.cache_on_arguments(expiration_time=3600)
+def __get_fas_grp_member(group='packager'):
+    ''' Retrieve from FAS the list of users in the packager group.
     '''
-    def __init__(self, fas_connection):
-        super(UserCache, self).__init__()
-        self.fas = fas_connection
+    fas = get_fas()
 
-    def __getitem__(self, username):
-        '''Retrieve a user for a username.
-
-        First read from the cache.  If not in the cache, refresh from
-        the server and try again.
-
-        If the user does not exist then, KeyError will be raised.
-        '''
-        username = username.strip()
-        if username not in self:
-            if not username:
-                # If the key is just whitespace, raise KeyError immediately,
-                # don't try to pull from fas
-                raise KeyError(username)
-            LOG.debug(_('refresh forced for %(user)s') % {'user':  username})
-            person = self.fas.person_by_username(username)
-            if not person:
-                # no value for this username
-                raise KeyError(username)
-            self[username] = person
-        return super(UserCache, self).__getitem__(username)
+    return fas.group_member(group)
 
 
-class StatusCache(dict):
-    def __init__(self, timeout=3600):
-        self.timeout = timeout
+@pkgdb.cache.cache_on_arguments(expiration_time=3600)
+def get_bz_email_user(username):
+    ''' Retrieve the bugzilla email associated to the provided username.
+    '''
+    fas = get_fas()
 
-    def __getitem__(self, status_id):
-        '''Return the other half of status from memcache or the database
-
-        :arg status_id: This can be either a statuscode or a statusname.
-            If it's a statusname, the statuscode will be returned.  If
-            it's a statusname, the statuscode will be returned.
-        '''
-        if isinstance(status_id, basestring):
-            # Have a statusname, looking for an id
-
-            # First ask memcache server for the value
-            mc_id = 'pkgdb:status:%s' % Hasher(status_id).hexdigest()
-            status_value = MEMCACHE.get(mc_id)
-
-            if not status_value:
-                status = session.query(StatusTranslation).filter(
-                    and_(
-                        StatusTranslationTable.c.language == 'C',
-                        StatusTranslationTable.c.statusname == status_id
-                    )
-                ).one()
-        else:
-            # Have an id, look for a statusname
-
-            # Ask memcache server for the value
-            mc_id = 'pkgdb:status:%s' % str(status_id)
-            status_value = MEMCACHE.get(mc_id)
-
-            if not status_value:
-                try:
-                    status = session.query(StatusTranslation).filter(and_(
-                        StatusTranslation.language == 'C',
-                        StatusTranslation.statuscodeid == status_id)).one()
-                except DataError:
-                    # If status_id was not an integer we get a DataError.  In
-                    # that case, we know we won't find the value we want
-                    status = None
-
-        if not status_value:
-            if not status:
-                raise KeyError(_('Unknown status: %(status)s') %
-                               {'status': status_id})
-
-            status_value = status.statuscodeid
-            # Save in memcache for the next status lookup
-            MEMCACHE.set(mc_id, status_value, self.timeout)
-
-        return status_value
-
-    def __setitem__(self, statusname, value):
-        raise TypeError(_('\'StatusCache\' object does not support item'
-                        ' assignment'))
-
-    def refresh_status(self):
-        '''Recache all the status types
-        '''
-        status_map = {}
-        for status in session.query(StatusTranslation).filter(
-                StatusTranslationlanguage == 'C').all():
-            status_map[Hasher(status.statusname).hexdigest()
-                       ] = status.statuscodeid
-            status_map[status.statuscodeid] = status.statusname
-        MEMCACHE.set_multi(status_map, key_prefix='pkgdb:status:',
-                           time=self.timeout)
+    return fas.person_by_username(username)
 
 
 def get_bz():
@@ -214,103 +86,59 @@ def get_bz():
     global _bugzilla
     if _bugzilla:
         return _bugzilla
+
     # Get a connection to bugzilla
-    bz_server = config.get('bugzilla.queryurl', config.get('bugzilla.url',
-                           'https://bugzilla.redhat.com'))
+    bz_server = pkgdb.APP.config['PKGDB_BUGZILLA_URL']
+    if not bz_server:
+        raise pkgdb.lib.PkgdbException('No PKGDB_BUGZILLA_URL configured')
     bz_url = bz_server + '/xmlrpc.cgi'
-    bz_user = config.get('bugzilla.user')
-    bz_pass = config.get('bugzilla.password')
+    bz_user = pkgdb.APP.config['PKGDB_BUGZILLA_USER']
+    if not bz_user:
+        raise pkgdb.lib.PkgdbException('No PKGDB_BUGZILLA_USER configured')
+    bz_pass = pkgdb.APP.config['PKGDB_BUGZILLA_PASSWORD']
+    if not bz_pass:
+        raise pkgdb.lib.PkgdbException(
+            'No PKGDB_BUGZILLA_PASSWORD configured')
+
     _bugzilla = RHBugzilla3(url=bz_url, user=bz_user, password=bz_pass,
                             cookiefile=None)
     return _bugzilla
 
 
-def custom_template_vars(new_vars):
-    return new_vars.update({'fas_cache': fas.cache})
+def _set_bugzilla_owner(username, pkg_name, collectn,
+        collectn_version, bzComment)=None:
+    '''Change the package owner
 
-
-def init_globals():
-    '''Initialize global variables.
-
-This is mostly connections to services like FAS, bugzilla, and loading
-    constants from the database.
+     :arg user_email: User email address to change the owner.
+     :arg pkg_name: Name of the package to change the owner.
+     :arg collectn: Collection name of the package.
+     :arg collectn_version: Collection version.
+     :kwarg bzComment: the comment of changes, if left to None, rely on a
+        default comment.
     '''
-    global fas, LOG, STATUS
-    # Things to do on startup
-    STATUS = StatusCache()
-    STATUS.refresh_status()
-    LOG = logging.getLogger('pkgdb.controllers')
+    bzComment = 'This package has changed ownership in the Fedora'\
+                ' Package Database.  Reassigning to the new owner'\
+                ' of this component.'
 
-    # Get a connection to the Account System server
-    fas_url = config.get('fas.url',
-                         'https://admin.fedoraproject.org/accounts/')
-    username = config.get('fas.username', 'admin')
-    password = config.get('fas.password', 'admin')
+    user_email = get_bz_email_user(username)
 
-    fas = AccountSystem(fas_url, username=username, password=password,
-                        cache_session=False)
-    fas.cache = UserCache(fas)
+    bzMail = '%s' % user_email
+    bzQuery = {}
+    bzQuery['product'] = collectn
+    bzQuery['component'] = pkg_name
+    bzQuery['bug_status'] = ['NEW', 'ASSIGNED', 'ON_DEV', 'ON_QA',
+            'MODIFIED', 'POST', 'FAILS_QA', 'PASSES_QA',
+            'RELEASE_PENDING']
+    bzQuery['version'] = collectn_version
+    if bzQuery['version'] == 'devel':
+        bzQuery['version'] = 'rawhide'
+    queryResults = get_bz().query(bzQuery) #pylint:disable-msg=E1101
 
-    view.variable_providers.append(custom_template_vars)
-
-
-def is_xhr():
-    '''Check if the request is coming from AJAX
-    '''
-
-    requested_with = request.headers.get('X-Requested-With')
-    return requested_with and requested_with.lower() == 'xmlhttprequest'
-
-
-# Modified from yumUtils rpmUtils.rpm2cpio code.  Need to work on it more
-def rpm2cpio(fdno, out=sys.stdout, bufsize=2048):
-    """Performs roughly the equivalent of rpm2cpio(8).
-       Reads the package from fdno, and dumps the cpio payload to out,
-       using bufsize as the buffer size."""
-    ts = rpmUtils.transaction.initReadOnlyTransaction()
-    hdr = ts.hdrFromFdno(fdno)
-    del ts
-
-    compr = hdr[rpm.RPMTAG_PAYLOADCOMPRESSOR] or 'gzip'
-    if compr == 'gzip':
-        f = gzip.GzipFile(None, 'rb', None, os.fdopen(fdno, 'rb', bufsize))
-        while 1:
-            tmp = f.read(bufsize)
-            if tmp == "":
-                break
-            out.write(tmp)
-        out.flush()
-        f.close()
-    elif compr == 'xz':
-        # This is a bit hacky
-        xz_cmd = subprocess.Popen(['/usr/bin/xzcat'],
-                                  stdout=subprocess.PIPE,
-                                  stdin=os.fdopen(fdno, 'rb', bufsize),
-                                  bufsize=bufsize,
-                                  shell=False)
-        while 1:
-            tmp = xz_cmd.stdout.read(bufsize)
-            if tmp == "":
-                break
-            out.write(tmp)
-        out.flush()
-    elif compr == 'bzip2':
-        f = os.fdopen(fdno, 'rb', bufsize)
-        decompressor = bz2.BZ2Decompressor()
-        while 1:
-            chunk = f.read(bufsize)
-            if chunk == "":
-                break
-            try:
-                tmp = decompressor.decompress(tmp)
-            except EOFError:
-                break
-            out.write(tmp)
-        out.flush()
-        f.close()
-    else:
-        raise rpmUtils.RpmUtilsError, \
-            'Unsupported payload compressor: "%s"' % compr
-
-__all__ = [LOG, MEMCACHE, STATUS, admin_grp, critpath_grps, fas, get_bz,
-           init_globals, is_xhr, pkger_grp, rpm2cpio, to_unicode]
+    for bug in queryResults:
+        if config.get('bugzilla.enable_modification', False):
+            bug.setassignee(assigned_to=bzMail, comment=bzComment)
+        else:
+            LOG.debug(_('Would have reassigned bug #%(bug_num)s'
+            ' from %(former)s to %(current)s') % {
+                'bug_num': bug.bug_id, 'former': bug.assigned_to,
+                'current': bzMail})
