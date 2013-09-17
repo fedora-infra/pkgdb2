ccdc87aaeabd3be9368d150a749d4b65892abca1
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-09-17 15:49:12 +0200

    Fix the unit-tests to query the right database using the appropriate session
    This explains why I was getting local data rather than the one generated for the tests.

--- tests/test_flask_api_extras.py
+++ tests/test_flask_api_extras.py
@@ -48,15 +48,16 @@ class FlaskApiExtrasTest(Modeltests):
 
         pkgdb.APP.config['TESTING'] = True
         pkgdb.SESSION = self.session
-        pkgdb.api.acls.SESSION = self.session
+        pkgdb.api.extras.SESSION = self.session
         self.app = pkgdb.APP.test_client()
+
         # Let's make sure the cache is empty for the tests
         pkgdb.cache.invalidate()
 
     def test_api_bugzilla(self):
         """ Test the api_bugzilla function.  """
 
-        pkgdb.cache.invalidate()
+        # Empty DB
         output = self.app.get('/api/bugzilla/')
         self.assertEqual(output.status_code, 200)
 
@@ -65,71 +66,108 @@ class FlaskApiExtrasTest(Modeltests):
 # Collection|Package|Description|Owner|Initial QA|Initial CCList
 # Backslashes (\) are escaped as \u005c Pipes (|) are escaped as \u007c
 
-Fedora|geany|IDE|toshio|pingou
-Fedora|guake|Drop down terminal|pingou|spot
-Fedora|python-gpgme|GPG module in python|toshio|
-Fedora|perl-foo|Foor in perl|group::perl-sig|
-Fedora|perl-bar|Bar in perl|group::perl-sig|
-Fedora|test|test|pingou|test
-Fedora|test2|test|test|"""
+"""
         self.assertEqual(output.data, expected)
 
         output = self.app.get('/api/bugzilla/?format=json')
         self.assertEqual(output.status_code, 200)
         data = json.loads(output.data)
-        expected = {u'bugzillaAcls': {u'Fedora': [
-            {u'geany':
-                {u'owner': u'toshio', u'cclist':
-                    {u'groups': [], u'people': [u'pingou']},
-                 u'qacontact': None,
-                 u'summary': u'IDE'}},
-            {u'guake':
-                {u'owner': u'pingou', u'cclist':
-                    {u'groups': [], u'people': [u'spot']},
-                 u'qacontact': None,
-                 u'summary': u'Drop down terminal'}},
-            {u'python-gpgme':
-                {u'owner': u'toshio', u'cclist':
-                    {u'groups': [], u'people': []},
-                 u'qacontact': None,
-                 u'summary': u'GPG module in python'}},
-            {u'perl-foo':
-                {u'owner': u'group::perl-sig', u'cclist':
-                    {u'groups': [], u'people': []},
-                 u'qacontact': None,
-                 u'summary': u'Foor in perl'}},
-            {u'perl-bar':
-                {u'owner': u'group::perl-sig', u'cclist':
-                    {u'groups': [], u'people': []},
-                 u'qacontact': None,
-                 u'summary': u'Bar in perl'}},
-            {u'test':
-                {u'owner': u'pingou', u'cclist':
-                    {u'groups': [], u'people': [u'test']},
-                 u'qacontact': None,
-                 u'summary': u'test'}},
-            {u'test2':
-                {u'owner': u'test', u'cclist':
-                    {u'groups': [], u'people': []},
-                 u'qacontact': None,
-                 u'summary': u'test'}}
-        ]}, u'title': u'Fedora Package Database -- Bugzilla ACLs'}
+        expected = {
+            u'bugzillaAcls': {},
+            u'title': u'Fedora Package Database -- Bugzilla ACLs'
+        }
+
+        self.assertEqual(data, expected)
+
+        # Filled DB
+        create_package_acl(self.session)
+
+        output = self.app.get('/api/bugzilla/')
+        self.assertEqual(output.status_code, 200)
+
+        expected = """# Package Database VCS Acls
+# Text Format
+# Collection|Package|Description|Owner|Initial QA|Initial CCList
+# Backslashes (\) are escaped as \u005c Pipes (|) are escaped as \u007c
+
+Fedora|guake|Top down terminal for GNOME|pingou|toshio
+Fedora|fedocal|A web-based calendar for Fedora|orphan|
+Fedora|geany|A fast and lightweight IDE using GTK2|pingou|"""
+        self.assertEqual(output.data, expected)
+
+        output = self.app.get('/api/bugzilla/?format=json')
+        self.assertEqual(output.status_code, 200)
+        data = json.loads(output.data)
+        expected = {
+            u'bugzillaAcls': {
+                u'Fedora': [
+                    {u'guake': {
+                        u'owner': u'pingou',
+                        u'cclist': {
+                            u'groups': [],
+                            u'people': [u'toshio']
+                        },
+                        u'qacontact': None,
+                        u'summary': u'Top down terminal for GNOME'
+                        }
+                    },
+                    {u'fedocal': {
+                        u'owner': u'orphan',
+                        u'cclist': {
+                            u'groups': [],
+                            u'people': []
+                        },
+                        u'qacontact': None,
+                        u'summary': u'A web-based calendar for Fedora'
+                        }
+                    },
+                    {u'geany': {
+                        u'owner': u'pingou',
+                        u'cclist': {
+                            u'groups': [],
+                            u'people': []
+                        },
+                        u'qacontact': None,
+                        u'summary': u'A fast and lightweight IDE using GTK2'
+                        }
+                    }
+                ]
+            },
+            u'title': u'Fedora Package Database -- Bugzilla ACLs'
+        }
 
         self.assertEqual(data, expected)
 
     def test_api_notify(self):
         """ Test the api_notify function.  """
 
+        # Empty DB
         output = self.app.get('/api/notify/')
         self.assertEqual(output.status_code, 200)
 
-        expected = """geany|toshio,pingou
-guake|pingou,spot
-python-gpgme|toshio
-perl-foo|group::perl-sig
-perl-bar|group::perl-sig
-test|test,pingou
-test2|test,pingou"""
+        expected = ""
+        self.assertEqual(output.data, expected)
+
+        output = self.app.get('/api/notify/?format=json')
+        self.assertEqual(output.status_code, 200)
+        data = json.loads(output.data)
+        expected = {
+            u'packages': [],
+            u'title': u'Fedora Package Database -- Notification List',
+            u'name': None,
+            u'version': None,
+            u'eol': False
+        }
+
+        self.assertEqual(data, expected)
+
+        # Filled DB
+        create_package_acl(self.session)
+
+        output = self.app.get('/api/notify/')
+        self.assertEqual(output.status_code, 200)
+
+        expected = """guake|pingou,toshio"""
         self.assertEqual(output.data, expected)
 
         output = self.app.get('/api/notify/?format=json')
@@ -139,19 +177,39 @@ test2|test,pingou"""
         expected = {
             u'title': u'Fedora Package Database -- Notification List',
             u'packages': [
-                {u'geany': [u'toshio', u'pingou']},
-                {u'guake': [u'pingou', u'spot']},
-                {u'python-gpgme': [u'toshio']},
-                {u'perl-foo': [u'group::perl-sig']},
-                {u'perl-bar': [u'group::perl-sig']},
-                {u'test': [u'test', u'pingou']},
-                {u'test2': [u'test', u'pingou']}
-            ], u'name': None, u'version': None, u'eol': False
+                {u'guake': [u'pingou', u'toshio']}
+            ],
+            u'name': None,
+            u'version': None,
+            u'eol': False
         }
         self.assertEqual(data, expected)
 
     def test_api_vcs(self):
         """ Test the api_vcs function.  """
+
+        # Empty DB
+        output = self.app.get('/api/vcs/')
+        self.assertEqual(output.status_code, 200)
+
+        expected = """# VCS ACLs
+# avail|@groups,users|rpms/Package/branch
+
+"""
+        self.assertEqual(output.data, expected)
+
+        output = self.app.get('/api/vcs/?format=json')
+        self.assertEqual(output.status_code, 200)
+        data = json.loads(output.data)
+        expected = {
+            u'packageAcls': {},
+            u'title': u'Fedora Package Database -- VCS ACLs'
+        }
+
+        self.assertEqual(data, expected)
+
+        # Filled DB
+        create_package_acl(self.session)
 
         output = self.app.get('/api/vcs/')
         self.assertEqual(output.status_code, 200)
@@ -159,19 +217,11 @@ test2|test,pingou"""
         expected = """# VCS ACLs
 # avail|@groups,users|rpms/Package/branch
 
-avail | @provenpackager,toshio,pingou | rpms/geany/master
-avail | @provenpackager,toshio,pingou | rpms/geany/f19
-avail | @provenpackager,pingou,spot | rpms/guake/master
-avail | @provenpackager,pingou | rpms/guake/f19
-avail | @provenpackager,toshio | rpms/python-gpgme/master
-avail | @provenpackager,toshio | rpms/python-gpgme/f19
-avail | @provenpackager,@perl-sig, | rpms/perl-foo/master
-avail | @provenpackager,@perl-sig, | rpms/perl-foo/f19
-avail | @provenpackager,@perl-sig, | rpms/perl-bar/master
-avail | @provenpackager,test,pingou | rpms/test/master
-avail | @provenpackager,test | rpms/test/f19
-avail | @provenpackager,test,pingou | rpms/test2/master
-avail | @provenpackager,test | rpms/test2/f19"""
+avail | @provenpackager,pingou | rpms/guake/f18
+avail | @provenpackager,pingou,toshio | rpms/guake/master
+avail | @provenpackager, | rpms/fedocal/f18
+avail | @provenpackager, | rpms/fedocal/master
+avail | @provenpackager, | rpms/geany/f18"""
         self.assertEqual(output.data, expected)
 
         output = self.app.get('/api/vcs/?format=json')
@@ -180,16 +230,16 @@ avail | @provenpackager,test | rpms/test2/f19"""
 
         expected = {
             u'packageAcls': {
-                u'f19': {
+                u'f18': {
                     u'commit': {
                         u'groups': [u'provenpackager'],
-                        u'people': [u'test']
+                        u'people': []
                     }
                 },
                 u'master': {
                     u'commit': {
                         u'groups': [u'provenpackager'],
-                        u'people': [u'test', u'pingou']
+                        u'people': []
                     }
                 }
             },
