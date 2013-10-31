d06dbc2f6b5608e1a8c3833aa57b46360c7ba235
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-10-31 18:55:41 +0100

    Another attempt at fixing the tests on jenkins

--- tests/test_flask_ui_packages.py
+++ tests/test_flask_ui_packages.py
@@ -127,10 +127,10 @@ class FlaskUiPackagesTest(Modeltests):
             self.assertEqual(output.status_code, 200)
             ## FIXME: this is the same problem as in test_api_package_new
             ## we need to look into this.
-            self.assertEqual(
+            self.assertTrue(
                 output.data.count(
                     '<td class="errors">This field is required.</td>'
-                ), 5 or 6)
+                ) in (5, 6))
 
             mock_func.get_packagers.return_value=['mclasen']
             mock_func.log.return_value = ''
