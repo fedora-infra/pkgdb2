5eb7423932765c0476ffac6ba0887bbef489933d
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-10-31 10:11:00 +0100

    Import the Collection API coverage

--- tests/test_flask_api_collection.py
+++ tests/test_flask_api_collection.py
@@ -31,12 +31,15 @@ import unittest
 import sys
 import os
 
+from mock import patch
+
 sys.path.insert(0, os.path.join(os.path.dirname(
     os.path.abspath(__file__)), '..'))
 
 import pkgdb
 from pkgdb.lib import model
-from tests import (Modeltests, FakeFasUser, create_collection)
+from tests import (Modeltests, FakeFasUser, FakeFasUserAdmin,
+                   create_collection, user_set)
 
 
 class FlaskApiCollectionTest(Modeltests):
@@ -51,63 +54,79 @@ class FlaskApiCollectionTest(Modeltests):
         pkgdb.api.collections.SESSION = self.session
         self.app = pkgdb.APP.test_client()
 
-    def test_collection_status(self):
+    @patch('pkgdb.packager_login_required')
+    def test_collection_status(self, login_func):
         """ Test the api_collection_status function.  """
+        login_func.return_value=None
 
-        output = self.app.post('/api/collection/F-18/status/')
-        self.assertEqual(output.status_code, 500)
-        data = json.loads(output.data)
-        self.assertEqual(
-            data,
-            {
-                "output": "notok",
-                "error_detail": [
-                    "collection_status: Not a valid choice",
-                    "collection_branchname: This field is required.",
-                ],
-                "error": "Invalid input submitted",
-            }
-        )
+        # Redirect as you are not admin
+        user = FakeFasUser()
 
-        data = {'collection_branchname': 'F-18',
-                'collection_status' : 'EOL'}
-        output = self.app.post('/api/collection/F-19/status/', data=data)
-        self.assertEqual(output.status_code, 500)
-        data = json.loads(output.data)
-        self.assertEqual(
-            data,
-            {
-                "output": "notok",
-                "error": "You're trying to update the wrong collection",
-            }
-        )
+        with user_set(pkgdb.APP, user):
+            output = self.app.post('/api/collection/F-18/status/')
+            self.assertEqual(output.status_code, 302)
+
+        user = FakeFasUserAdmin()
+
+        with user_set(pkgdb.APP, user):
+            output = self.app.post('/api/collection/F-18/status/')
+            self.assertEqual(output.status_code, 500)
+            data = json.loads(output.data)
+            self.assertEqual(
+                data,
+                {
+                    "output": "notok",
+                    "error_detail": [
+                        "collection_status: Not a valid choice",
+                        "collection_branchname: This field is required.",
+                    ],
+                    "error": "Invalid input submitted",
+                }
+            )
 
         data = {'collection_branchname': 'F-18',
                 'collection_status' : 'EOL'}
-        output = self.app.post('/api/collection/F-18/status', data=data)
-        self.assertEqual(output.status_code, 500)
-        data = json.loads(output.data)
-        self.assertEqual(
-            data,
-            {
-                "output": "notok",
-                "error": "You are not allowed to edit collections",
-            }
-        )
+        with user_set(pkgdb.APP, user):
+            output = self.app.post('/api/collection/F-19/status/', data=data)
+            self.assertEqual(output.status_code, 500)
+            data = json.loads(output.data)
+            self.assertEqual(
+                data,
+                {
+                    "output": "notok",
+                    "error": "You're trying to update the wrong collection",
+                }
+            )
+
+        data = {'collection_branchname': 'F-18',
+                'collection_status' : 'EOL'}
+        with user_set(pkgdb.APP, user):
+            output = self.app.post('/api/collection/F-18/status', data=data)
+            self.assertEqual(output.status_code, 500)
+            data = json.loads(output.data)
+            self.assertEqual(
+                data,
+                {
+                    "output": "notok",
+                    "error": 'Could not find collection "F-18"',
+                }
+            )
 
         create_collection(self.session)
 
-        #data = {'collection_branchname': 'F-18',
-                #'collection_status' : 'EOL'}
-        #output = self.app.post('/api/collection/F-18/status', data=data)
-        #self.assertEqual(output.status_code, 200)
-        #output = json.loads(output.data)
-        #self.assertEqual(output.keys(),
-                         #['output', 'messages'])
-        #self.assertEqual(output['output'], 'ok')
-        #self.assertEqual(len(output['messages']), 1)
-        #self.assertEqual(output['messages'][0],
-                         #'Collection updated to "EOL"')
+        data = {'collection_branchname': 'F-18',
+                'collection_status' : 'EOL'}
+        with user_set(pkgdb.APP, user):
+            output = self.app.post('/api/collection/F-18/status', data=data)
+            self.assertEqual(output.status_code, 200)
+            data = json.loads(output.data)
+            self.assertEqual(
+                data,
+                {
+                    "output": "ok",
+                    "messages": ['Collection updated to \"EOL\"'],
+                }
+            )
 
     def test_collection_list(self):
         """ Test the api_collection_list function.  """
@@ -148,26 +167,37 @@ class FlaskApiCollectionTest(Modeltests):
         self.assertEqual(output['collections'][0]['name'], 'Fedora')
         self.assertEqual(output['collections'][0]['version'], '17')
 
-    def test_collection_new(self):
+    @patch('pkgdb.packager_login_required')
+    def test_collection_new(self, login_func):
         """ Test the api_collection_new function.  """
+        login_func.return_value=None
 
-        output = self.app.post('/api/collection/new/')
-        self.assertEqual(output.status_code, 500)
-        data = json.loads(output.data)
-        self.assertEqual(
-            data,
-            {
-                "output": "notok",
-                "error_detail": [
-                    "collection_status: Not a valid choice",
-                    "collection_name: This field is required.",
-                    "collection_branchname: This field is required.",
-                    "collection_distTag: This field is required.",
-                    "collection_version: This field is required.",
-                ],
-                "error": "Invalid input submitted",
-            }
-        )
+        # Redirect as you are not admin
+        user = FakeFasUser()
+        with user_set(pkgdb.APP, user):
+            output = self.app.post('/api/collection/new/')
+            self.assertEqual(output.status_code, 302)
+
+        user = FakeFasUserAdmin()
+
+        with user_set(pkgdb.APP, user):
+            output = self.app.post('/api/collection/new/')
+            self.assertEqual(output.status_code, 500)
+            data = json.loads(output.data)
+            self.assertEqual(
+                data,
+                {
+                    "output": "notok",
+                    "error_detail": [
+                        "collection_status: Not a valid choice",
+                        "collection_name: This field is required.",
+                        "collection_branchname: This field is required.",
+                        "collection_distTag: This field is required.",
+                        "collection_version: This field is required.",
+                    ],
+                    "error": "Invalid input submitted",
+                }
+            )
 
         data = {
             'collection_name':'EPEL',
@@ -176,31 +206,42 @@ class FlaskApiCollectionTest(Modeltests):
             'collection_status':'ACTIVE',
             'collection_distTag':'.el6',
         }
+        with user_set(pkgdb.APP, user):
+            output = self.app.post('/api/collection/new/', data=data)
+            self.assertEqual(output.status_code, 500)
+            data = json.loads(output.data)
+            self.assertEqual(
+                data,
+                {
+                    "error": "Invalid input submitted",
+                    "error_detail": [
+                        "collection_status: Not a valid choice"
+                    ],
+                    "output": "notok"
+                }
+            )
 
-        output = self.app.post('/api/collection/new/', data=data)
-        self.assertEqual(output.status_code, 500)
-        output = json.loads(output.data)
-        self.assertEqual(output.keys(),
-                         ['output', 'error_detail', 'error'])
-        self.assertEqual(output['output'], 'notok')
-        self.assertEqual(output['error'], 'Invalid input submitted')
-        self.assertEqual(output['error_detail'],
-                         ['collection_status: Not a valid choice'])
-
-        ## Need to find out how to set flask.g.fas_user
-        #data = {
-            #'collection_name':'EPEL',
-            #'collection_version':'6',
-            #'collection_branchname':'EL-6',
-            #'collection_status':'Active',
-            #'collection_distTag':'.el6',
-        #}
-        #output = self.app.post('/api/collection/new/', data=data)
-        #print output.data
-        #self.assertEqual(output.status_code, 500)
-        #output = json.loads(output.data)
-        #self.assertEqual(output.keys(),
-                         #['error', 'error_detail', 'output'])
+        # Need to find out how to set flask.g.fas_user
+        data = {
+            'collection_name':'EPEL',
+            'collection_version':'6',
+            'collection_branchname':'EL-6',
+            'collection_status':'Active',
+            'collection_distTag':'.el6',
+        }
+        with user_set(pkgdb.APP, user):
+            output = self.app.post('/api/collection/new/', data=data)
+            self.assertEqual(output.status_code, 200)
+            data = json.loads(output.data)
+            self.assertEqual(
+                data,
+                {
+                    "messages": [
+                        "Collection \"EL-6\" created"
+                    ],
+                    "output": "ok"
+                }
+            )
 
 
 if __name__ == '__main__':
