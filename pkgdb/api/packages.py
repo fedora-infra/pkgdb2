321bec5deceebd91290429b916b1efe59a7b0d77
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-11-17 18:48:06 +0100

    Retrict the branch returned to the active branches

--- pkgdb/api/packages.py
+++ pkgdb/api/packages.py
@@ -410,7 +410,8 @@ def api_package_info(pkg_name=None):
             pkg_clt=pkg_clt,
         )
         output['output'] = 'ok'
-        output['packages'] = [pkg.to_json() for pkg in packages]
+        output['packages'] = [pkg.to_json() for pkg in packages
+                              if pkg.collection.status != 'EOL']
     except NoResultFound:
         output['output'] = 'notok'
         output['error'] = 'Package: %s not found' % pkg_name
