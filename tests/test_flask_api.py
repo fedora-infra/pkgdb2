00d5452935dc3456ab666863f4fc1e825c539eaa
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-11-19 12:18:43 +0100

    Add test for the top level code of the API namespace (including /api/version)

---
+++ tests/test_flask_api.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2013  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2, or (at your option) any later
+# version.  This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY expressed or implied, including the
+# implied warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.  You
+# should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# Any Red Hat trademarks that are incorporated in the source
+# code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission
+# of Red Hat, Inc.
+#
+
+'''
+pkgdb tests for the Flask API regarding collections.
+'''
+
+__requires__ = ['SQLAlchemy >= 0.7']
+import pkg_resources
+
+import json
+import unittest
+import sys
+import os
+
+from mock import patch
+
+sys.path.insert(0, os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), '..'))
+
+import pkgdb
+from pkgdb import APP
+from pkgdb.lib import model
+from tests import (Modeltests, FakeFasUser, FakeFasUserAdmin,
+                   create_package_acl, user_set)
+
+
+
+class FlaskApiTest(Modeltests):
+    """ Flask API tests. """
+
+    def setUp(self):
+        """ Set up the environnment, ran before every tests. """
+        super(FlaskApiTest, self).setUp()
+
+        pkgdb.APP.config['TESTING'] = True
+        pkgdb.SESSION = self.session
+        pkgdb.api.SESSION = self.session
+        self.app = pkgdb.APP.test_client()
+
+    def test_api_version(self):
+        """ Test the api_version function.  """
+
+        output = self.app.get('/api/version')
+        self.assertEqual(output.status_code, 200)
+        data = json.loads(output.data)
+        self.assertEqual(data['version'], pkgdb.__api_version__)
+        self.assertEqual(data.keys(), ['version'])
+
+
+if __name__ == '__main__':
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(FlaskApiTest)
+    unittest.TextTestRunner(verbosity=2).run(SUITE)
