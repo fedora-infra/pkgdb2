4f899364a312c8fa6b3612db69a48650e587936e
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-11-26 20:50:46 +0100

    Ok so let's keep the API doc in pkgdb itself until we can resolve the import

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,7 @@
 
 import sys, os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+#sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 #from pkgdb import __version__
 
