3dbc0a24b7016c310f02d8fb4e81d206b84c0172
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-11-27 19:27:58 +0100

    Fix the runtest script

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #PKGDB_CONFIG=../tests/pkgdb_test.cfg PYTHONPATH=pkgdb nosetests \
+
 PYTHONPATH=pkgdb ./nosetests \
---with-coverage --cover-erase --cover-package=pkgdb $*
+--with-coverage --cover-erase --cover-package=pkgdb2 $*
