c1674c4fa3d58def17106f88ed5e8bb6c1f21c07
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-12-13 09:28:37 +0100

    Add now a pep8 fix on ui.collections

--- pkgdb2/ui/collections.py
+++ pkgdb2/ui/collections.py
@@ -31,9 +31,11 @@ import pkgdb2.lib as pkgdblib
 from pkgdb2 import SESSION, APP, is_admin
 from pkgdb2.ui import UI
 
+
 ## Some of the object we use here have inherited methods which apparently
 ## pylint does not detect.
 # pylint: disable=E1101
+
 
 @UI.route('/collections/')
 @UI.route('/collections/page/<int:page>/')
