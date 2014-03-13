8c17fe9579aca5d00b64986e235207c6ef1f22ee
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-03-13 17:00:49 +0100

    Let the packager info endpoint of the API filter for or out ACLs for EOL collections

--- pkgdb2/api/packagers.py
+++ pkgdb2/api/packagers.py
@@ -62,6 +62,12 @@ User's ACL
     :arg packagername: String of the packager name.
     :kwarg acls: One or more ACL to filter the ACLs retrieved. Options are:
         ``approveacls``, ``commit``, ``watchbugzilla``, ``watchcommits``.
+    :kwarg eol: a boolean to specify whether to filter for or out
+        EOL collections. Defaults to None.
+        If True, it will return results only for EOL collections.
+        If False, it will return results only for non-EOL collections.
+        If None, it will not filter the results on the status of the
+        collection.
     :kwarg page: The page number to return (useful in combination to limit).
     :kwarg limit: An integer to limit the number of results, defaults to
         250, maximum is 500 (acls).
@@ -146,6 +152,7 @@ User's ACL
 
     packagername = flask.request.args.get('packagername', None) or packagername
     acls = flask.request.args.getlist('acls', None)
+    eol = flask.request.args.getlist('eol', None)
 
     pkg_acl = pkgdblib.get_status(SESSION, 'pkg_acl')['pkg_acl']
     for acl in acls:
@@ -167,6 +174,7 @@ User's ACL
             SESSION,
             packager=packagername,
             acls=acls,
+            eol=eol,
             page=page,
             limit=limit,
             count=count)
@@ -181,6 +189,7 @@ User's ACL
                 SESSION,
                 packager=packagername,
                 acls=acls,
+                eol=eol,
                 count=True)
 
             output['page_total'] = total_acl / limit
