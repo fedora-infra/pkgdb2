da8fed1a07ed2010d79becae8ed86d58a6170a98
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-06-16 12:14:29 +0200

    Prevent the same user to do multiple times the same request

--- pkgdb2/lib/model/__init__.py
+++ pkgdb2/lib/model/__init__.py
@@ -1627,6 +1627,12 @@ class AdminAction(BASE):
                               default=datetime.datetime.utcnow,
                               onupdate=sa.func.now())
 
+    __table_args__ = (
+        sa.UniqueConstraint(
+            'user', 'action', 'status', 'package_id', 'collection_id'),
+    )
+
+
     package = relation("Package")
     collection = relation(
         "Collection",
