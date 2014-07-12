19e302b8763cd7b46c962053f92dd17017cb619d
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-07-12 16:09:45 +0200

    Add a dedicated method to deny all the pending ACLs

--- pkgdb2/ui/acls.py
+++ pkgdb2/ui/acls.py
@@ -531,3 +531,31 @@ def pending_acl_approve():
         flask.flash(str(err), 'error')
 
     return flask.redirect(flask.url_for('.pending_acl'))
+
+
+@UI.route('/acl/pending/deny', methods=['POST'])
+@packager_login_required
+def pending_acl_deny():
+    ''' Deny all the pending acls for the user logged in. '''
+    pending_acls = pkgdblib.get_pending_acl_user(
+        SESSION, flask.g.fas_user.username)
+    try:
+        for acl in pending_acls:
+            pkgdblib.set_acl_package(
+                SESSION,
+                pkg_name=acl['package'],
+                pkg_branch=acl['collection'],
+                pkg_user=acl['user'],
+                acl=acl['acl'],
+                status='Denied',
+                user=flask.g.fas_user
+            )
+
+        SESSION.commit()
+        flask.flash('All ACLs denied')
+        # Let's keep this in although we should never see it
+    except pkgdblib.PkgdbException, err:  # pragma: no cover
+        SESSION.rollback()
+        flask.flash(str(err), 'error')
+
+    return flask.redirect(flask.url_for('.pending_acl'))
