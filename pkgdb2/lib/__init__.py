80eb05070fdc9e911f693f209408301108406f51
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-08-12 22:26:44 +0200

    Expose the AdminAction.get() method in the internal API

--- pkgdb2/lib/__init__.py
+++ pkgdb2/lib/__init__.py
@@ -2115,3 +2115,16 @@ def set_monitor_package(session, pkg_name, status, user):
             raise PkgdbException('Could not update monitoring status.')
 
     return msg
+
+
+def get_admin_action(session, action_id):
+    """ For a given Admin Action identifier, return the Admin Action object
+    having this identifier.
+
+    :arg session: the session with which to connect to the database.
+    :arg action_id: The identifier of the admin action to retrieve.
+    :returns: an Admin Action object having the specified identifier.
+    :rtype: AdminAction()
+
+    """
+    return model.AdminAction.get(session, action_id)
