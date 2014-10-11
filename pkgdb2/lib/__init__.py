37fd84dcad29192cdd0a1f048d8555ca90561462
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-10-11 12:50:48 +0200

    Check that the package exists before checking if the user is allowed to change it

--- pkgdb2/lib/__init__.py
+++ pkgdb2/lib/__init__.py
@@ -1818,17 +1818,17 @@ def set_monitor_package(session, pkg_name, status, user):
 
     """
 
+    try:
+        package = model.Package.by_name(session, pkg_name)
+    except NoResultFound:
+        raise PkgdbException('No package found by this name')
+
     pkger = has_acls(
         session, user.username, pkg_name, ['commit', 'approveacls'])
     if not (pkger or pkgdb2.is_pkgdb_admin(user)):
         raise PkgdbException(
             'You are not allowed to update the monitor flag on this package'
         )
-
-    try:
-        package = model.Package.by_name(session, pkg_name)
-    except NoResultFound:
-        raise PkgdbException('No package found by this name')
 
     msg = 'Monitoring status un-changed'
     if package.monitor != status:
