a7c6ad98e71bf6d0740a1ddd3f6a57ced28b5b6f
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-11-21 10:41:29 +0100

    Include the api_monitored in the API documentation at /api/

--- pkgdb2/api/__init__.py
+++ pkgdb2/api/__init__.py
@@ -113,6 +113,7 @@ def api():
     api_extras_notify_all = load_doc(extras.api_notify_all)
     api_extras_vcs = load_doc(extras.api_vcs)
     api_extras_pendingacls = load_doc(extras.api_pendingacls)
+    api_extras_monitored = load_doc(extras.api_monitored)
 
     return flask.render_template(
         'api.html',
@@ -141,7 +142,8 @@ def api():
         extras=[
             api_extras_bugzilla, api_extras_critpath,
             api_extras_notify, api_extras_notify_all,
-            api_extras_vcs, api_extras_pendingacls
+            api_extras_vcs, api_extras_pendingacls,
+            api_extras_monitored
         ]
     )
 
