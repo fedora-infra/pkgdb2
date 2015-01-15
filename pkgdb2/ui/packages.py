ead2ca8c6eb1fec7885926042a37eb6b764a86a6
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-01-15 15:45:44 +0100

    Allow requester to view their request (and edit it)

--- pkgdb2/ui/packages.py
+++ pkgdb2/ui/packages.py
@@ -343,10 +343,11 @@ def package_request_edit(package, action_id):
 
     # Check user is the pkg/pkgdb admin
     if not is_pkgdb_admin(flask.g.fas_user) and not pkgdblib.has_acls(
-            SESSION, flask.g.fas_user.username, package, 'approveacls'):
+            SESSION, flask.g.fas_user.username, package, 'approveacls') \
+            and not admin_action.user == flask.g.fas_user.username:
         flask.flash(
-            'Only package adminitrators (`approveacls`) can review pending '
-            'branch requests', 'errors')
+            'Only package adminitrators (`approveacls`) and requester can '
+            'review pending branch requests', 'errors')
         return flask.redirect(
             flask.url_for('.package_info', package=package)
         )
