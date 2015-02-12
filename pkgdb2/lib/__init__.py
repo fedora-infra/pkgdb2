1a020233e16e05988df1468bfae9222763025f89
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-02-12 16:08:22 +0100

    Fix changing the status of the request after granting it

--- pkgdb2/lib/__init__.py
+++ pkgdb2/lib/__init__.py
@@ -1752,7 +1752,7 @@ def add_new_branch_request(session, pkg_name, clt_from, clt_to, user):
             clt_to.branchname, user.username)
 
         # The branch is created, so the action has been approved
-        action.status = 'Approved'
+        action._status = 'Approved'
         session.add(action)
 
     return msg
