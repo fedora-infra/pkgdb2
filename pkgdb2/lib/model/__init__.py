025d390bac32c4e7fd59e1adf5e05026bceb395f
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-05-19 14:09:50 +0200

    Be more generous about how the DB can store a boolean in a text field

--- pkgdb2/lib/model/__init__.py
+++ pkgdb2/lib/model/__init__.py
@@ -1236,7 +1236,7 @@ class Package(BASE):
         return session.query(
             cls
         ).filter(
-            Package.monitor.in_(['1', 'nobuild'])
+            Package.monitor.in_(['1', 'true', 'True', 'nobuild'])
         ).order_by(
             Package.name
         ).all()
