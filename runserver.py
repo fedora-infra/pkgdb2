a8a1c7acdb34d9a56187f6d20f652914c41a4a1a
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-10-06 09:53:35 +0200

    Update the runserver script to offer more options

--- runserver.py
+++ runserver.py
@@ -4,14 +4,38 @@
 __requires__ = ['SQLAlchemy >= 0.8', 'jinja2 >= 2.4']
 import pkg_resources
 
+import argparse
 import sys
-from werkzeug.contrib.profiler import ProfilerMiddleware
+import os
 
 from pkgdb2 import APP
 APP.debug = True
 
-if '--profile' in sys.argv:
+parser = argparse.ArgumentParser(
+    description='Run the packages2 app')
+parser.add_argument(
+    '--config', '-c', dest='config',
+    help='Configuration file to use for packages.')
+parser.add_argument(
+    '--debug', dest='debug', action='store_true',
+    default=False,
+    help='Expand the level of data returned.')
+parser.add_argument(
+    '--profile', dest='profile', action='store_true',
+    default=False,
+    help='Profile the packages2 application.')
+parser.add_argument(
+    '--port', '-p', default=5000,
+    help='Port for the flask application.')
+
+args = parser.parse_args()
+
+if args.profile:
+    from werkzeug.contrib.profiler import ProfilerMiddleware
     APP.config['PROFILE'] = True
     APP.wsgi_app = ProfilerMiddleware(APP.wsgi_app, restrictions=[30])
 
-APP.run()
+if args.config:
+    os.environ['PKGS_CONFIG'] = args.config
+
+APP.run(port=int(args.port))
