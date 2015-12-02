3fa4482cb6cab7cfd9d284ecae5b529649d022ca
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-12-02 16:15:13 +0100

    Invert the conditional to make it easier to read and understand

--- pkgdb2/lib/__init__.py
+++ pkgdb2/lib/__init__.py
@@ -2486,8 +2486,10 @@ def edit_action_status(
                 'You are not allowed to edit this request')
     elif action_status in ['Awaiting Review', 'Blocked']:
         # Requester can re-set 'request.unretire' to 'Awaiting Review'
-        if (not pkg_admin and not pkgdb_admin and not
+        if (pkg_admin or pkgdb_admin or
                 (requester and admin_action.action == 'request.unretire')):
+            pass
+        else:
             raise PkgdbException(
                 'You are not allowed to review this request')
     elif action_status in ['Obsolete']:
